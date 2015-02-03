85be6001c28744ae22da24e100b3ad3f4fa59047
Eric Peterson <ericp@math.berkeley.edu>
2015-02-02 18:06:10 -0800

    various ‘plans’ for future code

---
+++ app/routes/workspace-manip.js
@@ -0,0 +1,32 @@
+
+// app/routes/workspace-manip.js
+
+var Workspace = require('../models/workspace.js');
+
+module.exports = function(app) {
+	//app.get('/workspace/:id/children', app.isLoggedIn, function(req, res) {
+
+	//});
+
+	// REQUEST INDIVIDUAL WORKSPACE DATA =======================================
+	//app.get('/workspace/:id', app.isLoggedIn, function(req, res) {
+
+	//});
+
+	//app.put('/workspace/:id', app.isLoggedIn, function(req, res) {
+
+	//});
+
+	//app.delete('/workspace/:id', app.isLoggedIn, function(req, res) {
+
+	//});
+
+	//app.post('/newworkspace', app.isLoggedIn, function(req, res) {
+
+	//});
+
+	//app.get('/workspaces', app.isLoggedIn, function(req, res) {
+		// want something like:
+		// Workspace.find({ users : req.user._id }, ...);
+	//});
+}
