f8807774219657b6d78b67a1a09662e6076bca08
Eric Peterson <ericp@math.berkeley.edu>
2015-02-18 12:17:36 -0800

    insert some stock code that prevents some node overlap

--- public/javascript/global.js
+++ public/javascript/global.js
@@ -45,7 +45,8 @@ $(document).ready(function() {
     force = d3.layout.force()
               .charge(-180) // how forceful the repositioning is
               .linkDistance(120) // relaxed length of edge springs
-              .size([width, height]);
+              .size([width, height])
+              .on('tick', forceTick);
 
     force
         .nodes([])
@@ -179,6 +180,38 @@ function clickBulb(d, i) {
 
     console.log("Unhandled click state!");
     return;
+}
+
+// from mbostock
+function collide(node) {
+  var r = node.radius + 16,
+      nx1 = node.x - r,
+      nx2 = node.x + r,
+      ny1 = node.y - r,
+      ny2 = node.y + r;
+  return function(quad, x1, y1, x2, y2) {
+    if (quad.point && (quad.point !== node)) {
+      var x = node.x - quad.point.x,
+          y = node.y - quad.point.y,
+          l = Math.sqrt(x * x + y * y),
+          r = node.radius + quad.point.radius;
+      if (l < r) {
+        l = (l - r) / l * .5;
+        node.x -= x *= l;
+        node.y -= y *= l;
+        quad.point.x += x;
+        quad.point.y += y;
+      }
+    }
+    return x1 > nx2
+        || x2 < nx1
+        || y1 > ny2
+        || y2 < ny1;
+  };
+}
+
+function forceTick(e) {
+    node.selectAll('circle').each(collide(.5));
 }
 
 function drawGraphCallback () {
